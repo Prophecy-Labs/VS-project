326681209ea37498a02c058f854a00f0eae3a5fb
PepegaSlayer <129308099+PepegaSlayer@users.noreply.github.com>
2023-12-05 15:38:42 +0500

    Create project

---
+++ SchoolWebServiceProphecyLabs/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using SchoolWebServiceProphecyLabs.Data;
+
+namespace SchoolWebServiceProphecyLabs.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public DataBase dataBase = new DataBase();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Register([FromBody] Userdata data) 
+        {        
+            return Ok(dataBase.Insert(data.login,data.email,data.password));
+        }
+
+        [HttpPost]
+        public IActionResult LogIn([FromBody] Userdata data)
+        {         
+            return Ok(dataBase.Login(data.login, data.password)); 
+        }
+    }
+    public class Userdata
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public string email { get; set; }
+    }
+
+}
---
+++ SchoolWebServiceProphecyLabs/Data/DataBase.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SchoolWebServiceProphecyLabs.Data
+{
+    public class DataBase
+    {
+        public static SqlConnection SqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=X:\VS projects\SchoolWebServiceProphecyLabs\SchoolWebServiceProphecyLabs\Data\Database1.mdf;Integrated Security=True");
+
+        public string Insert(string login, string email, string password) {
+            if (Find(login))
+                return "User already exist";
+            string result = "Something goes wrong";
+            string qs = $"insert into Users(login,password,email) values ('{login}','{password}','{email}')";
+            SqlCommand cmd = new SqlCommand(qs, SqlConnection);
+            SqlConnection.Open();
+            if (cmd.ExecuteNonQuery() == 1)
+                result = "successful";
+            SqlConnection.Close();
+            return result;
+        }
+
+        public string Login(string login, string password) {
+            string result = "user doesnt exist";
+            string qs = $"SELECT * FROM Users WHERE login = '{login}';";
+            SqlCommand cmd = new SqlCommand(qs, SqlConnection);
+            SqlConnection.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                reader.Read();
+                if (reader.GetValue(1).ToString() == password)
+                    result = "successful";
+                else
+                    result = "wrong password";
+            }
+            SqlConnection.Close();
+            return result;
+        }
+
+        public bool Find(string login) {
+            bool result = false;
+            SqlCommand cmd = new SqlCommand($"SELECT * FROM Users WHERE login = '{login}';", SqlConnection);
+            SqlConnection.Open();
+            if (cmd.ExecuteReader().HasRows)
+                result = true;
+            SqlConnection.Close();
+            return result;
+        }
+    }
+}
